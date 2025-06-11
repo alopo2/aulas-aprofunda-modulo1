b89ad8efffe9432bdfdde7ad5ed2c105d85648a4
Angela Carvalho <angelalopo@outlook.com>
2025-06-11 20:52:12 -0300

    adiciona os exercicios da aula 2

--- semana1/server.js
+++ semana1/server.js
@@ -1,8 +1,29 @@
-import express from 'express';
+import express from 'express'; 
+import cors from 'cors';
+import dados from './dados.json' with { type: 'json' };
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+const router = express.Router();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const mensagem = router.get('/', (_, res) => {
+    res.send('Olá, Mundo!');
+});
+
+const ola = router.get('/ola', (_, res) => {
+    res.send('Olá, Mundo!');
+});
+
+const alunas = router.get('/alunas', (_, res) => {
+    res.json(dados);
+});
+
+app.use(mensagem);
+app.use(ola);
+app.use(alunas);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
